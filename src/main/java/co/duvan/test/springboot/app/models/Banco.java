28931e5a3672737b2d4551d73eb3d903fd52ec82
DUVAN0829 <duvancasgonz29@gmail.com>
2025-09-12 08:57:19 -0500

    Añade dos clases models 'Banco' y 'Cuenta' con configuración básica

---
+++ src/main/java/co/duvan/test/springboot/app/models/Banco.java
@@ -0,0 +1,46 @@
+package co.duvan.test.springboot.app.models;
+
+public class Banco {
+
+    //* Vars
+    private Long id;
+    private String nombre;
+    private int totalTransferencias;
+
+    //* Constructors
+    public Banco() {
+    }
+
+    public Banco(Long id, String nombre, int totalTransferencias) {
+        this.id = id;
+        this.nombre = nombre;
+        this.totalTransferencias = totalTransferencias;
+    }
+
+    //* Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTotalTransferencias() {
+        return totalTransferencias;
+    }
+
+    public void setTotalTransferencias(int totalTransferencias) {
+        this.totalTransferencias = totalTransferencias;
+    }
+
+
+}
---
+++ src/main/java/co/duvan/test/springboot/app/models/Cuenta.java
@@ -0,0 +1,70 @@
+package co.duvan.test.springboot.app.models;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Cuenta {
+
+    //* vars
+    private Long id;
+    private String persona;
+    private BigDecimal saldo;
+
+    //* Constructors
+    public Cuenta() {
+    }
+
+    public Cuenta(Long id, String persona, BigDecimal saldo) {
+        this.id = id;
+        this.persona = persona;
+        this.saldo = saldo;
+    }
+
+    //* Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPersona() {
+        return persona;
+    }
+
+    public void setPersona(String persona) {
+        this.persona = persona;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    //* Methods
+    public void debito(BigDecimal monto) {
+        this.saldo = this.saldo.subtract(monto);
+    }
+
+    public void credito(BigDecimal monto) {
+        this.saldo = saldo.add(monto);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cuenta cuenta = (Cuenta) o;
+        return Objects.equals(id, cuenta.id) && Objects.equals(persona, cuenta.persona) && Objects.equals(saldo, cuenta.saldo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, persona, saldo);
+    }
+
+}
