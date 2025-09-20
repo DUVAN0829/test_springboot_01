149c96f79340e4b16d6bfc9f5efd7e3cbda75eec
DUVAN0829 <duvancasgonz29@gmail.com>
2025-09-20 09:29:07 -0500

    Implementa 'CrudRepository' en repositories 'BancoRepository', 'CuentaRepository'

--- src/main/java/co/duvan/test/springboot/app/repositories/BancoRepository.java
+++ src/main/java/co/duvan/test/springboot/app/repositories/BancoRepository.java
@@ -1,15 +1,16 @@
 package co.duvan.test.springboot.app.repositories;
 
 import co.duvan.test.springboot.app.models.Banco;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface BancoRepository {
+public interface BancoRepository extends CrudRepository<Banco, Long> {
 
-    Banco findById(Long id);
-
-    List<Banco> findAll();
-
-    void update(Banco banco);
+//    Banco findById(Long id);
+//
+//    List<Banco> findAll();
+//
+//    void update(Banco banco);
 
 }
--- src/main/java/co/duvan/test/springboot/app/repositories/CuentaRepository.java
+++ src/main/java/co/duvan/test/springboot/app/repositories/CuentaRepository.java
@@ -1,16 +1,17 @@
 package co.duvan.test.springboot.app.repositories;
 
 import co.duvan.test.springboot.app.models.Cuenta;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface CuentaRepository {
+public interface CuentaRepository extends CrudRepository<Cuenta, Long> {
 
-    List<Cuenta> findAll();
-
-    Cuenta findById(Long id);
-
-    void update(Cuenta cuenta);
+//    List<Cuenta> findAll();
+//
+//    Cuenta findById(Long id);
+//
+//    void update(Cuenta cuenta);
 
 
 
